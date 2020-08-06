09e974908f4637d6b2f012a131035b1f7d440eae
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-06 17:51:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,25 @@
+require "pry"
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :artist_name, :name 
   @@all = []
 
+  def self.create 
+    @song = self.new 
+    @@all << @song
+    return @song 
+  end
+  
+  def self.new_by_name(songs_name)
+    @songs_name = songs_name
+    
+    return @songs_name
+  end
+
+  
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
